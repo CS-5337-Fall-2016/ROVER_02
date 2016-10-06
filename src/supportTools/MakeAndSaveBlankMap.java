ec0994d7daf6c2e3913a753b9956d923072411d8
Davis <davislouie90@gmail.com>
2016-10-06 08:51:21 -0700

    Bugfixes

--- src/supportTools/MakeAndSaveBlankMap.java
+++ src/supportTools/MakeAndSaveBlankMap.java
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-package supportTools;
-
-import java.io.IOException;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import common.Coord;
-import common.MapTile;
-import common.PlanetMap;
-import common.RoverLocations;
-import common.ScanMap;
-import common.ScienceLocations;
-import enums.Science;
-import enums.Terrain;
-import json.MyWriter;
-
-public class MakeAndSaveBlankMap {
-
-	public static void main(String[] args) throws IOException {
-		int mapWidth = 100;
-		int mapHeight = 60;
-		
-		String fileName = "Map" + mapWidth + "x" + mapHeight + "blank.txt";
-			
-		SwarmMapInit mapInit = new SwarmMapInit(fileName, mapWidth, mapHeight, new PlanetMap(mapWidth, mapHeight), new RoverLocations(), new ScienceLocations());
-
-		mapInit.saveToDisplayTextFile(fileName);
-		mapInit.printToDisplayTextFile();
-				
-	}
-}
-=======
 package supportTools;
 
 import java.io.IOException;
@@ -64,4 +30,3 @@ public class MakeAndSaveBlankMap {
 				
 	}
 }
->>>>>>> swarmserver/master
